9e65c93212c0374ff69ca3c9c5d0087407038e65
Piotr Walczyszyn <piotr.walczyszyn@gmail.com>
2012-10-14 22:27:36 +0200

    advanced restore from tag instead of branch

--- restore-advanced.sh
+++ restore-advanced.sh
@@ -1,6 +1,12 @@
 cd www
-git checkout advanced
-git merge --strategy=ours master
+git checkout CLEAN_ADVANCED
 git checkout master
-git merge advanced
-git push
+git reset --hard CLEAN_ADVANCED
+git push --force
+
+#cd www
+#git checkout advanced
+#git merge --strategy=ours master
+#git checkout master
+#git merge advanced
+#git push
